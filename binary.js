fd158d9d3b0a3f3cce70240e4e95ef3b97d03cdb
imuhammmadh <imuhammmadh@gmail.com>
2023-10-17 23:10:40 +0530

    .

--- binary.js
+++ binary.js
@@ -49,3 +49,37 @@ function upperBound(arr, target) {
     }
     return ans
 } console.log(upperBound([2, 3, 6, 7, 8, 8, 11, 11, 11, 12], 6));
+
+function searchInsertPosition(arr, target) {
+    let n = arr.length
+    let low = 0
+    let high = n - 1
+    let ans = n
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if (arr[mid] >= target) {
+            ans = mid
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return ans
+}
+
+function floor(arr, target) {
+    let n = arr.length
+    let low = 0
+    let high = n - 1
+    let ans = n
+    while (low <= high) {
+        let mid = Math.ceil((low + high) / 2)
+        if (arr[mid] <= target) {
+            ans = arr[mid]
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return ans
+} console.log(floor([10, 20, 30, 40], 25));
