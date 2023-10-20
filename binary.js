bccde9d26a5788960d79e1af6dacba6cfdd9a7b1
imuhammmadh <imuhammmadh@gmail.com>
2023-10-20 08:31:05 +0530

    .

--- binary.js
+++ binary.js
@@ -90,3 +90,61 @@ function firstNlast(arr, target) {
     if (lb == n || arr[lb] !== target) return [-1, -1]
     return [lb, upperBound(arr, target) - 1]
 } console.log(firstNlast([2, 4, 6, 8, 8, 8, 11, 13], 8));
+
+function rotatedArraySearch(arr, target) {
+    let n = arr.length
+    let low = 0
+    let high = n - 1
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if (arr[mid] == target) return mid
+        else if (arr[low] <= arr[mid]) {
+            if (arr[low] <= target && arr[mid] >= target) {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        }
+        else {
+            if (arr[high] >= target && arr[mid] <= target) {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    return -1
+}
+console.log(rotatedArraySearch([7, 8, 9, 1, 2, 3, 4, 5, 6], 1));
+
+function rotatedArraySearchII(arr, k) {
+    let n = arr.length;
+    let low = 0
+    let high = n - 1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (arr[mid] == k) return true;
+        if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+            low = low + 1;
+            high = high - 1;
+            continue;
+        }
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        else {
+            if (arr[mid] <= k && k <= arr[high]) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+    }
+    return false;
+}
