1c5a319f70f875ecbc50b300b10d8b3c2b947fe1
imuhammmadh <imuhammmadh@gmail.com>
2023-12-07 14:06:34 +0530

    .

--- binary.js
+++ binary.js
@@ -166,3 +166,44 @@ function rotatedMinimum(arr) {
     }
     return ans
 } console.log(rotatedMinimum([7, 8, 9, 10, 1, 2, 3, 4, 5]));
+
+function elmOnce(arr) {
+    let n = arr.length
+    let low = 1
+    let high = n - 2
+    if (n == 1) return arr[0]
+    if (arr[0] !== arr[1]) return arr[0]
+    if (arr[n - 1] !== arr[n - 2]) return arr[n - 1]
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if (arr[mid] !== arr[mid + 1] && arr[mid] !== arr[mid - 1]) {
+            return arr[mid]
+        }
+        if ((mid % 2 !== 0 && arr[mid] == arr[mid - 1]) || (mid % 2 == 0 && arr[mid] == arr[mid + 1])) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return -1
+} console.log(elmOnce([1, 1, 2, 2, 3, 3, 6, 5, 5]));
+
+function peakElement(arr) {
+    let n = arr.length
+    let low = 1
+    let high = n - 2
+    if (n == 0) return 0
+    if (arr[0] > arr[1]) return 0
+    if (arr[n - 1] > arr[n - 2]) return n - 1
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) {
+            return mid
+        } else if (arr[mid] > arr[mid - 1]) {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return -1
+} console.log(peakElement([0, 1, 2, 3, 4, 5, 6, 8, 7]));
