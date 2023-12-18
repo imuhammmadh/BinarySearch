8059cc9c10e79e2ff8e530df4cc8939f290bea15
imuhammmadh <imuhammmadh@gmail.com>
2023-12-18 13:41:58 +0530

    .

--- binary.js
+++ binary.js
@@ -207,3 +207,19 @@ function peakElement(arr) {
     }
     return -1
 } console.log(peakElement([0, 1, 2, 3, 4, 5, 6, 8, 7]));
+
+function floorSqrt(n) {
+    let low = 1
+    let high = n
+    let ans = 1
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2)
+        if ((mid * mid) <= n) {
+            ans = mid
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return ans
+} console.log(floorSqrt(28));
