289f08a60d7421c7ee151735afee05e2ebb28878
imuhammmadh <imuhammmadh@gmail.com>
2023-12-20 18:53:49 +0530

    .

--- binary.js
+++ binary.js
@@ -208,18 +208,28 @@ function peakElement(arr) {
     return -1
 } console.log(peakElement([0, 1, 2, 3, 4, 5, 6, 8, 7]));
 
-function floorSqrt(n) {
+function floorSqrt(n, m) {
+    function f(mid, n, m) {
+        let ans = 1
+        for (let i = 1; i <= n; i++) {
+            ans = ans * mid
+            if (ans > m) return 2
+        }
+        if (ans == m) return 1
+        return 0
+    }
     let low = 1
-    let high = n
+    let high = m
     let ans = 1
     while (low <= high) {
         let mid = Math.floor((low + high) / 2)
-        if ((mid * mid) <= n) {
-            ans = mid
+        if (f(mid, n, m) == 1) {
+            return mid
+        } else if (f(mid, n, m) == 0) {
             low = mid + 1
         } else {
             high = mid - 1
         }
     }
-    return ans
-} console.log(floorSqrt(28));
+    return -1
+} console.log(floorSqrt(3, 27));
